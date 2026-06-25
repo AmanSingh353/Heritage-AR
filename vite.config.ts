702bd2d3c6c904dbfcab1abdbc404277de319bc4
AmanSingh353 <asr9773692353@gmail.com>
2026-06-26 01:28:49 +0530

    Updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
@@ -17,6 +18,7 @@ export default defineConfig({
         },
       },
     }),
+    nitro(),
     react(),
   ],
   resolve: {

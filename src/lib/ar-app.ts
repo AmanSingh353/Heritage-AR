f3158acc7893282dccfbe617a2ed975019f430a2
AmanSingh353 <asr9773692353@gmail.com>
2026-06-26 12:01:52 +0530

    Updated the download link

--- src/lib/ar-app.ts
+++ src/lib/ar-app.ts
@@ -1,2 +1,2 @@
-export const AR_APP_URL = "/Heritage-AR.apk";
+export const AR_APP_URL = "https://drive.google.com/uc?export=download&id=11a_op_lJmie3UoJgZB32URGUKfl1Or3h";
 export const AR_APP_FILENAME = "Heritage-AR.apk";
